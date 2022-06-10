deeed5cf7bdfc2f5cff2ce88251655b1c9a5bcab
Matheus Scramignon <matheus.mignon@gmail.com>
2022-06-10 13:58:04 -0300

    Add export_pivot option

--- source/modulus_aggregator/constants.py
+++ source/modulus_aggregator/constants.py
@@ -3,3 +3,4 @@ VERSION = 0.1
 # Error Messages for tags exporter
 RUN_PATH_NOT_FOUND = "Experiment run path was not found."
 EXPORT_ERROR = "Error while exporting tags."
+WIDE_DF_NOT_POSSIBLE = "It was not possible to convert tags data to wide form. Exporting in standard form..."
